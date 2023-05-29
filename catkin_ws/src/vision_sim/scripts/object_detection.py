e7d27168db2b178709d0c2e4b42f4a5a78a37eb5
holoom <abdelhaleemsaad981@gmail.com>
2023-05-29 21:46:48 +0200

    object detection added

--- catkin_ws/src/vision_sim/scripts/object_detection.py
+++ catkin_ws/src/vision_sim/scripts/object_detection.py
@@ -6,16 +6,32 @@ import cv2
 from cv_bridge import CvBridge
 import numpy as np
 import torch
-
+import matplotlib.pyplot as plt
 model = torch.hub.load('ultralytics/yolov5','custom',path='/home/haleem/catkin_ws/src/vision_sim/scripts/best.pt',_verbose=False)
 
 def image_callback(msg):
     # Convert ROS message to OpenCV image
     cv_image = bridge.compressed_imgmsg_to_cv2(msg)
-    # Object detection using Yolov5
-    result = model(cv_image)
-    cv2.imshow('Object detection', result)
     
+    rgb_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
+    # rgb_image = Image.open(cv_image)
+
+    # Object detection using Yolov5
+    # cv2.imwrite('test.jpeg',cv_image)
+    results = model(rgb_image,size=416)
+    # results.show()
+    print(results)
+
+    output = results.xyxy
+    for obj in output:
+        xmin, ymin, xmax, ymax, confidence, class_id = obj[0]
+        xmin, ymin, xmax, ymax,class_id = int(xmin),int(ymin),int(xmax),int(ymax), int(class_id)
+        # print(f'xmin: {xmin}, ymin: {ymin}, xmax: {xmax}, ymax: {ymax}, confidence: {confidence}, class_id: {class_id}')
+        cv2.rectangle(cv_image, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
+        cv2.putText(cv_image, f'{results.names[class_id]} : {confidence:.2f}', (xmin, ymin - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+
+    # Display the image with the bounding boxes
+    cv2.imshow('YOLOv5 Results', cv_image)
     cv2.waitKey(1)
     
 if __name__ == '__main__':
