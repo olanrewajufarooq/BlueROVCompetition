db2ae68909d408d259aff9a0db4d9e397ab3293a
Farooq Olanrewaju <olanrewajufarooq@yahoo.com>
2023-06-21 12:07:20 +0200

    Adding the video extracting codes

---
+++ VideoToPic/vid_to_pic.py
@@ -0,0 +1,25 @@
+import cv2
+
+vidcap = cv2.VideoCapture('Inspection_pipe.mkv')
+
+
+def getFrame(sec):
+
+    vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
+    hasFrames,image = vidcap.read()
+    
+    if hasFrames:
+        cv2.imwrite(f"ExtractedImages/image{count}.jpg", image)
+    
+    # save frame
+    return hasFrames
+
+sec = 0
+frameRate = 0.5 #//it will capture image in each 0.5 second
+count=1
+success = getFrame(sec)
+while success:
+    count = count + 1
+    sec = sec + frameRate
+    sec = round(sec,2)
+    success = getFrame(sec)
